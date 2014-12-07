cf4804efc038f8dd6c20ac7be3f646fade81a9d7
Te Vallee <astronomemusic@gmail.com>
2014-12-06 16:22:00 -1000

    basic layout done. now needs grids

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,4 +1,25 @@
 $(document).ready(function () {
+  $('body').addClass('clearfix');
+  $('<div></div>', {
+    id: 'color_grid',
+    text: 'COLOR GRID HERE'
+  }).appendTo('#controls');
+
+  $('<button></button>', {
+    id: 'erase',
+     text: 'erase' 
+  }).appendTo('#controls');
+
+   $('<button></button>', {
+    id: 'clear',
+     text: 'button'
+  }).appendTo('#controls');
+
+   $('<div></div>', {
+    id: 'pixel_grid',
+    text: 'PIXEL GRID THING HERE'
+   }).appendTo('#artboard');
+
   function pixelPainter(width, height) {
     
   }
