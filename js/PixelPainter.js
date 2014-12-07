1ad7f9464531a01f2c7a54b31500cd57d9e70897
Te Vallee <astronomemusic@gmail.com>
2014-12-06 21:18:23 -1000

    pixel grid and clear button functional

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,47 +1,63 @@
 $(document).ready(function () {
-  $('body').addClass('clearfix');
-  $('<div></div>', {
-    id: 'color_picker',
-    text: 'COLOR GRID HERE'
-  }).appendTo('#controls');
+  function PixelPainter(width, height) {
+    $('body').addClass('clearfix');
 
-  $('<button></button>', {
-    id: 'erase',
-     text: 'erase' 
-  }).appendTo('#controls');
+    $('<div></div>', {
+      id: 'color_picker'
+    }).appendTo('#controls');
+    // GRID FOR COLOR PICKER
+    for (var j = 0; j < 10; j++) {
+      for(var i=0; i<6; i++) {
+        $('<div></div>', {
+          class: 'cells'
+        }).appendTo('#color_picker');
+      }
+    };
 
-   $('<button></button>', {
-    id: 'clear',
-     text: 'clear'
-  }).appendTo('#controls');
+    $('<button></button>', {
+      id: 'erase',
+      text: 'erase' 
+    }).appendTo('#controls');
 
-   $('<div></div>', {
-    id: 'pixel_grid',
-    text: 'PIXEL GRID THING HERE'
-   }).appendTo('#artboard');
-   // COLOR PICKER TO GRID
-    $("#color_picker").click(function(){
+    $('<button></button>', {
+      id: 'clear',
+      text: 'clear'
+    }).appendTo('#controls');
+
+    $('<div></div>', {
+      id: 'pixel_grid'
+    }).appendTo('#artboard');
+
+    // GRID FOR ARTBOARD
+    for (var k = 0; k < height; k++) {
+      for(var l=0; l<width; l++) {
+        $('<div></div>', {
+          class: 'grid_cells'
+        }).appendTo('#pixel_grid');
+      }
+    };
+
+    // COLOR PICKER TO GRID
+    $(".cells").click(function(){
       var $array = ['background-color'];
       var $this = $(this);
       $.each( $array , function(item, value){
-        $('#pixel_grid').on('click', function() {
-
-          $("#pixel_grid").css(value, $this.css(value));
+        $('.grid_cells').on('click', function() {
+          $(this).css(value, $this.css(value));
         });
       });
     });
-   // ERASE EVENT LISTENER
-   $('button#erase').on('click', function() {
-    console.log('erase clicked');
-   });
-   // CLEAR EVENT LISTENER
-   $('button#clear').on('click', function() {
-    $('#pixel_grid').css('backgroundColor', '#FFFFFF');
-    console.log('clear clicked');
-   });
 
-  function pixelPainter(width, height) {
-    this.width = width;
-    this.height = height;
-  }
-});
+    // ERASE EVENT LISTENER
+    $('button#erase').on('click', function() {
+      console.log('erase clicked');
+    });
+
+    // CLEAR EVENT LISTENER
+    $('button#clear').on('click', function() {
+      $('.grid_cells').css('backgroundColor', '#FFFFFF');
+      console.log('clear clicked');
+    });
+  }// END PIXELPAINTE7
+  var pixelPainter = PixelPainter(16,14);
+}); // END
